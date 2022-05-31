eb7c19cdaa6e7dd00bc9e7d57f240b32334a68f5
Subhan <squadiloglu@gmail.com>
2022-05-31 22:02:43 +0400

    test+demopages

---
+++ demopages/tests.py
@@ -0,0 +1,11 @@
+from django.test import Client, TestCase
+
+
+class IndexTestCase(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+
+    def test_get_index_endpoint(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b"This is service 1")
---
+++ demopages/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+
+
+def index(response):
+    return HttpResponse("This is service 1")
